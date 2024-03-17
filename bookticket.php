5d7d034ce9ecdc1c6f19bb31741c5628179aae04
Nitin <nitingojiya2000@gmail.com>
2024-03-17 15:20:22 +0530

    nitin book

--- bookticket.php
+++ bookticket.php
@@ -22,7 +22,25 @@ if(isset($_POST['book']))
     $result=mysqli_query($con,$sql);
     if($result)
     {
-    header("location:home.php");
+     ?>
+     <h1><A href="">Your Ticket Confirm Booking </A></h1>
+     <h1><A href="home.php">Back Home</A></h1>
+     <table>
+        <tr>
+        <th>Name</th>
+        <th>Number of Ticket</th>
+        <th>Total Rs.</th>
+        <th>Date</th>
+        </tr>
+        <tr>
+        <th><?php echo $name ?></th>
+        <th><?php echo $ticket ?></th>
+        <th><?php echo $tot ?></th>
+        <th><?php echo $date ?></th>
+        </tr>
+     </table>
+     <?php   
+
     }
 }
 
